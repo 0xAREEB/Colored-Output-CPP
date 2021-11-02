96bab9ef8b48a707c407d94618c42f640ed2f4f5
0xAREEB <93572817+0xAREEB@users.noreply.github.com>
2021-11-02 13:59:54 +0500

    Add files via upload

---
+++ colored.cpp
@@ -0,0 +1,23 @@
+/*
+	Name: Simple Template for Colored Console(Output)
+	Copyright: 0xAREEB
+	Author: Arieb
+	Date: 02/11/21 13:45
+*/
+
+#include <iostream>
+#include <stdlib.h> // STEP 1: Include This :)
+
+using namespace std;
+
+int main(){
+	//HOW TO USE:  system("Color AB"); -> A is BACKGROUND COLOR  and  B is TEXT COLOR
+	
+	//SOME COLOR CODES: 0-Black, 1-Blue, 2-Green, 3-Aqua, 4-Red, 5-Purple, 6-Yellow, 7-White, 8-Gray, 9-Light-Blue, A-Light-Green,
+	//B-Light-Aqua, C-Light-Red, D-Light-Purple, E-Light-Yellow, F-Bright-White
+	
+	system("Color 8F"); // Grey-BACKGROUND and Bright-White-TEXT
+	
+	
+	return 0;
+}
